cbe22ff395bd1e67372ca8b6338d458dbd8f56cc
feneme7370 <marascofederico95@gmail.com>
2024-02-01 22:16:58 -0300

    beta 1.1.1

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,8 +3,15 @@ import axios from "axios";
 // al llamar a api, tomara la url referenciada, sirve para services
 const api = axios.create({
     // baseURL : 'https://www.thecocktaildb.com/api/json/v1/1'
-    baseURL : 'http://localhost:8000/api'
-    // baseURL : 'https://www.tumenuqr.up.railway.app/api'
+    // baseURL : 'http://localhost:8000/api'
+    baseURL : 'https://tumenuqr.femaser.com/api',
+    // baseURL : 'https://www.tumenuqr.up.railway.app/api
+    headers : {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        // 'origin': 'http://www.conxn.co.in',
+        // 'Access-Control-Allow-Origin': '*'
+    }
 })
 
 export default api
