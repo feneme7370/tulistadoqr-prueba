25f12e4e8bd02f8c300dc2395d4b635c3b6f4132
feneme7370 <marascofederico95@gmail.com>
2024-02-21 20:43:18 -0300

    beta 1.3.1 version completa

--- src/stores/config.js
+++ src/stores/config.js
@@ -3,8 +3,8 @@ import { defineStore } from "pinia";
 export const useConfigStore = defineStore( 'apiConfig', () => {
 
     const urlFront = 'http://localhost:5137/'
-    const urlBack = 'http://localhost:8000/'
-    // const urlBack = 'https://tumenuqr.femaser.com/'
+    // const urlBack = 'http://localhost:8000/'
+    const urlBack = 'https://tumenuqr.femaser.com/'
  
     return {
         urlBack,
