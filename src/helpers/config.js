76693100e81d3ffd86fed2df4d33c5c79f3b3f2a
feneme7370 <marascofederico95@gmail.com>
2024-03-07 17:19:55 -0300

    Version V 3.1.1

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,7 +1,7 @@
 // url
 export const urlBack = () => {
     return 'https://tulistadoqr.femaser.com/'
-    // return 'http://localhost:8000/'
+    // return 'http://localhost:8000/' 
 }
 
 export const urlFront = () => {
