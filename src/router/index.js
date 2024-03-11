ce9fe2f9ddffb11c7b911bd6134e6f7541209d91
feneme7370 <marascofederico95@gmail.com>
2024-03-11 16:05:13 -0300

    Version V 3.2.1 demo2 incluida

--- src/router/index.js
+++ src/router/index.js
@@ -9,16 +9,6 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    {
-      path: '/2-demo1',
-      name: '2-demo1',
-      component: () => import('@/views/2-demo1/IndexView.vue')
-    },
-    {
-      path: '/3-demo2',
-      name: '3-demo2',
-      component: () => import('@/views/3-demo2/IndexView.vue')
-    },
   ]
 })
 
