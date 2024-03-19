f408786590db728521e394f5beeeabb6eed6c131
feneme7370 <marascofederico95@gmail.com>
2024-03-19 14:11:27 -0300

    v.1.1.2 estilos

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import CompanyView from '@/views/CompanyView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,15 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/company/:id',
+      name: 'company',
+      component: CompanyView
+    },
+    {
+      path: '/:catchAll(.*)',
+      redirect: '/'
+    }
   ]
 })
 
